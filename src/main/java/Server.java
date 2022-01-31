63024f9c11feaecdd88358e24c0329ee705e3014
asf2007 <leha.flyagin@mail.ru>
2022-01-31 15:00:19 +0300

    home sc

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -6,19 +6,23 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args)  {
         System.out.println("Server started!");
         int port = 8089;
         while (true){
-            ServerSocket serverSocket = new ServerSocket(port);
+
+           try (ServerSocket serverSocket = new ServerSocket(port);
             Socket clientSocket = serverSocket.accept();
             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            System.out.println("New connection accepted");
-            final String name = in.readLine();
-            System.out.println(String.format("user %s, port is %d", name, clientSocket.getPort()));
-            out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
-            serverSocket.close();
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+               System.out.println("New connection accepted");
+               final String name = in.readLine();
+               System.out.println(String.format("user %s, port is %d", name, clientSocket.getPort()));
+               out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
+               serverSocket.close();
+           } catch (IOException e) {
+               e.printStackTrace();
+           }
         }
     }
 }
